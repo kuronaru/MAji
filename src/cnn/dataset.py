00d11339dca4911dd0e947b0e05c89b956b97c52
kuronaru <lujiajun_99@outlook.com>
2024-05-13 21:22:07 +0800

    New model: Tile Classifier
    This a residual network based CNN model used for Mahjong tile classification. The raw dataset "raw_data.zip" is collected from Maj-soul. The trained model is saved as "/data/model/model_tile_classifier.pt". Use resize_image.py to standardize raw data first and train the model with tile_classifier_train.py
    
    feature: resize raw data to 60*40; build dataset TileDataset; neural network TileClassifier; TileClassifier training program.

--- src/cnn/dataset.py
+++ src/cnn/dataset.py
@@ -33,7 +33,51 @@ class MajDataset(Dataset):
         img_path = self.img_paths[index]
         label = self.labels[index]
         image = Image.open(img_path)
-        image = self.transform(image)
+        if (self.transform):
+            image = self.transform(image)
         label = torch.Tensor([label])
-        # print(image, label)
+        return image, label
+
+
+class TileDataset(Dataset):
+    def __init__(self, path, n_img=8, start_index=0, transform=None) -> None:
+        super(TileDataset, self).__init__()
+        self.directory = path  # ../../data/tiles
+
+        img_paths = []
+        labels = []
+        for dir_name in os.listdir(self.directory):
+            directory = os.path.join(self.directory, dir_name)
+            if (os.path.isdir(directory)):
+                label = 0  # 0-36
+                if (dir_name.endswith("m")):
+                    label = int(dir_name[0])
+                elif (dir_name.endswith("p")):
+                    label = int(dir_name[0]) + 10
+                elif (dir_name.endswith("s")):
+                    label = int(dir_name[0]) + 20
+                elif (dir_name.endswith("z")):
+                    label = int(dir_name[0]) + 29
+
+                for i in range(n_img):
+                    img_name = "%02d.jpg" % (i + start_index)  # 00-07
+                    img_path = os.path.join(directory, img_name)
+                    if (os.path.exists(img_path)):
+                        img_paths.append(img_path)
+                        labels.append(label)
+
+        self.img_paths = img_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, index):
+        img_path = self.img_paths[index]
+        label = self.labels[index]
+        image = Image.open(img_path)
+        if (self.transform):
+            image = self.transform(image)
+        label = torch.Tensor([label])
         return image, label
